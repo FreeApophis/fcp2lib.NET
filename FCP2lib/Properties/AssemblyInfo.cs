ed29362001514b71d655b2b902d2ce57cf66e6fb
Apophis <apophis@apophis.ch>
2009-02-23 17:15:02 +0000

    Missing Properties File
    git-svn-id: https://svn.origo.ethz.ch/fcp2lib/trunk@10 f56a5a65-e1b4-4d35-b160-e61c0a6877f2

---
+++ FCP2lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FCP2lib")]
+[assembly: AssemblyDescription("Freenet Client Protocol 2.0 Implementation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("apophis.ch")]
+[assembly: AssemblyProduct("FCP2lib")]
+[assembly: AssemblyCopyright("Copyright 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible(false)]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion("0.9.0.0")]
