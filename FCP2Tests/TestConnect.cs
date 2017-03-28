819915a636c796f13458794583adec0fc99c5df7
Thomas Bruderer <apophis@apophis.ch>
2017-03-28 21:21:34 +0200

    Second Test downloading a file

--- FCP2Tests/TestConnect.cs
+++ FCP2Tests/TestConnect.cs
@@ -9,7 +9,7 @@ namespace FCP2Tests
     public class TestConnect
     {
         bool _hello = false;
-        FCP2.EventArgs.NodeHelloEventArgs result;
+        FCP2.EventArgs.NodeHelloEventArgs simpleResult;
 
         [TestMethod]
         public void SimpleConnect()
@@ -26,32 +26,89 @@ namespace FCP2Tests
 
             Assert.IsTrue(_hello);
 
-            Assert.AreEqual("2.0", result?.FcpVersion);
-            Assert.AreEqual("Fred", result?.Node);
+            Assert.AreEqual("2.0", simpleResult?.FcpVersion);
+            Assert.AreEqual("Fred", simpleResult?.Node);
 
             protocol.Disconnect();
         }
 
         private void Protocol_NodeHelloEvent(object sender, FCP2.EventArgs.NodeHelloEventArgs e)
         {
-            result = e;
+            simpleResult = e;
             _hello = true;
         }
+
+        FCP2.EventArgs.DataFoundEventArgs dataFound;
+        FCP2.EventArgs.AllDataEventArgs allData;
+        System.Text.StringBuilder testData = new System.Text.StringBuilder();
+
 
         [TestMethod]
         public void Test2()
         {
             var protocol = new FCP2Protocol("test-client");
             protocol.ClientHello();
-            protocol.NodeDataEvent += Protocol_NodeDataEvent;
-            protocol.ClientGet("USK@yGvITGZzrY1vUZK-4AaYLgcjZ7ysRqNTMfdcO8gS-LY,-ab5bJVD3Lp-LXEQqBAhJpMKrKJ19RnNaZMIkusU79s,AQACAAE/toad/56/activelink.png", "Toad-Active-Link");
 
-            System.Threading.Thread.Sleep(5000);
+            protocol.DataFoundEvent += Protocol_DataFoundEvent;
+            protocol.AllDataEvent += Protocol_AllDataEvent;
+
+            const string TestIdentifier = "test-file";
+            protocol.ClientGet("CHK@khy~PN5-P-Ze18SdPfb~eMVCcWv1RTDNaP3ketO3TwQ,-kcC5FBaNN7wZtcHgZGObkxk9221BL5r~wbbVTUhyHc,AAMC--8/test.txt", TestIdentifier);
+
+            int retry = 1000;
+            while (--retry > 0 && !Ready())
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+
+            const int TestSize = 16;
+            const string TestContentType = "text/plain";
+            const string TestData = "this is a test\r\n";
+
+            Assert.IsNotNull(allData);
+            Assert.IsNotNull(dataFound);
+
+            Assert.AreEqual(TestSize, dataFound.Datalength);
+            Assert.AreEqual(TestIdentifier, dataFound.Identifier);
+            Assert.IsFalse(dataFound.Global);
+            Assert.AreEqual(TestContentType, dataFound.ContentType);
+
+            Assert.IsTrue(allData.StartupTime <= allData.CompletionTime);
+
+            Assert.AreEqual(TestSize, allData.Datalength);
+            Assert.AreEqual(TestIdentifier, allData.Identifier);
+            Assert.AreEqual(TestData, testData.ToString());
+
+            protocol.Disconnect();
         }
 
-        private void Protocol_NodeDataEvent(object sender, FCP2.EventArgs.NodeDataEventArgs e)
+        private bool Ready()
         {
-            Trace.WriteLine("HERE");
+            return allData != null && dataFound != null;
+        }
+
+        private void Protocol_AllDataEvent(object sender, FCP2.EventArgs.AllDataEventArgs e)
+        {
+            Trace.WriteLine("Protocol_AllDataEvent");
+            var stream = e.GetStream();
+            var buffer = new byte[1024];
+
+            var bytesToRead = e.Datalength;
+            while (bytesToRead > 0)
+            {
+                var bytesRead = stream.Read(buffer, 0, (int)Math.Min(bytesToRead, buffer.Length));
+                bytesToRead -= bytesRead;
+                testData.Append(System.Text.Encoding.UTF8.GetString(buffer).TrimEnd('\0'));
+            }
+
+            allData = e;
+        }
+
+        private void Protocol_DataFoundEvent(object sender, FCP2.EventArgs.DataFoundEventArgs e)
+        {
+            Trace.WriteLine("Protocol_DataFoundEvent");
+
+            dataFound = e;
         }
     }
 }
